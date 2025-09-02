27f71b4cf117d5e2a1cfc9da20dc4e21c831f862
Tim Roberts <tim.roberts@shielded.io>
2025-09-02 16:05:17 +0100

    Update license headers to use `midnight-js` repo name

--- platform-js/platform-js/test/effect/ContractAddress.test.ts
+++ platform-js/platform-js/test/effect/ContractAddress.test.ts
@@ -1,5 +1,5 @@
 /*
- * This file is part of platform-js.
+ * This file is part of midnight-js.
  * Copyright (C) 2025 Midnight Foundation
  * SPDX-License-Identifier: Apache-2.0
  * Licensed under the Apache License, Version 2.0 (the "License");
