a4623bc6bf5bf321b30a02cf7be896eb0f1b4b1d
Tim Roberts <tim.roberts@shielded.io>
2025-09-04 20:55:07 +0100

    Remove process.exit() call

--- compact-js/compact-js-command/src/effect/internal/command.ts
+++ compact-js/compact-js-command/src/effect/internal/command.ts
@@ -65,7 +65,6 @@ const reportCausableError: (err: any) => Effect.Effect<void, never> =
       ) as Doc.AnsiDoc
     }
     yield* Console.log(Doc.render(errorDoc, {style: 'pretty'}));
-    process.exit(1); // Terminate with non-zero exit code on any causable error.
   });
 
 /**
