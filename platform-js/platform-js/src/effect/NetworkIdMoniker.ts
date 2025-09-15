7bb9a8e6a3574dcd361760a47575cfa2eb94879c
Szymon Paluchowski <szymon.paluchowski@iohk.io>
2025-09-15 16:29:07 +0200

    merge fix

--- platform-js/platform-js/src/effect/NetworkIdMoniker.ts
+++ platform-js/platform-js/src/effect/NetworkIdMoniker.ts
@@ -13,26 +13,13 @@
  * limitations under the License.
  */
 
-<<<<<<<< HEAD:testkit-js/testkit-js-e2e/src/unshielded-types.ts
-import type { ImpureCircuitId, MidnightProviders } from '@midnight-ntwrk/midnight-js-types';
-
-import { CompiledUnshielded } from './contract';
-
-export type UnshieldedContract = CompiledUnshielded.Contract<undefined>;
-
-export type UnshieldedContractCircuits = ImpureCircuitId<UnshieldedContract>;
-
-export type UnshieldedContractProviders = MidnightProviders<UnshieldedContractCircuits>;
-
-export const createUnshieldedContract = () : UnshieldedContract => new CompiledUnshielded.Contract({});
-========
 import { Brand, Option } from 'effect';
 
 const NETWORK_ID_REGEXP = /^[a-zA-Z0-9-]+$/;
 
 /**
  * A name, handle, or tag representing a familiar identifier given to an instance of a Midnight network.
- * 
+ *
  * @category models
  */
 export type NetworkIdMoniker = Brand.Branded<string, 'NetworkIdMoniker'>;
@@ -47,4 +34,3 @@ export const NetworkIdMoniker = Brand.refined<NetworkIdMoniker>(
       : Option.some(Brand.error(`Source string '${source}' is not a valid network identifier`));
   }
 );
->>>>>>>> main:platform-js/platform-js/src/effect/NetworkIdMoniker.ts
