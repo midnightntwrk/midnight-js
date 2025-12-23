2b21d38aa70abe52f6a1a2bef163b57a14977e0d
Szymon Paluchowski <szymon.paluchowski@iohk.io>
2025-12-23 17:45:21 +0100

    cleaner config

--- packages/contract-builder/src/config/PrivateStateConfig.ts
+++ packages/contract-builder/src/config/PrivateStateConfig.ts
@@ -13,20 +13,20 @@
  * limitations under the License.
  */
 
+import { type ContractProviders } from '@midnight-ntwrk/midnight-js-contracts';
+
 export interface PrivateStateConfig<TPrivateState> {
   stateId?: string;
   initialState: TPrivateState;
-  autoSave?: boolean;
   debug?: boolean;
 }
 
 export interface ConnectWithPrivateStateOptions {
   contractAddress: string;
   privateStateId: string;
-  providers: any;
+  providers: ContractProviders;
 }
 
 export const DEFAULT_PRIVATE_STATE_CONFIG = {
-  autoSave: true,
   debug: false
 };
